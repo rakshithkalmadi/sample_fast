426b763ac72d103f9b43b634039194a03f468c58
Rakshith Kalmadi <58518378+rakshithkalmadi@users.noreply.github.com>
2024-12-23 17:16:32 +0530

    Init

---
+++ main.py
@@ -0,0 +1,37 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.post("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    price = item.price+18
+    return {"item_id": item_id,"item_name": item.name,  "price":price}
+
+@app.get("/calculator")
+def calc(x: int,y: int,operator,task: Union[str, None] = None):
+    if task:
+        return {"Hello"}
+    if operator == "+":
+        return {"result":x+y}
+    else:
+        return {"result":x-y}       
